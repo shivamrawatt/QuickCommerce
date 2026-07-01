5b3c71ddbb0a8fab00b0882cf946f6b50ac62017
Shivam Rawat <shivamrawatt054@gmail.com>
2026-07-01 13:27:15 +0530

    test 5

--- backend-node/server.js
+++ backend-node/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 console.log("hello shivam, how are you doing today!!")
 console.log("hello shivam, how are you doing today what about the learning ci/cd")
 console.log("congratulations your ci/cd is working great ")
-console.log("docker bhai khush hua  test 3")
+console.log("docker bhai khush hua  test 5  build error resolved")
 
 const PORT = process.env.PORT || 3000;
 
